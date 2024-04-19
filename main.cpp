68ae29a7c995209ec6bb81e656110fa915de6c51
Mateusz <slomkam7@gmail.com>
2024-04-19 13:44:30 +0200

    add basic classes

--- main.cpp
+++ main.cpp
@@ -2,17 +2,57 @@
 #include <fmt/core.h>
 #include <SFML/Graphics.hpp>
 
-class Scene {
+struct Context {
+    sf::RenderWindow* window;
+    float timeDelta;
+};
 
+class Rect : Object {
+protected:
+    sf::RectangleShape s;
+
+public:
+
+    Rect(sf::Vector2f initialPos, sf::Vector2f size) : Object(initialPos) {
+        s = sf::RectangleShape(size);
+    }
+
+    auto draw(Context ctx) -> void {
+        auto shape = sf::RectangleShape(size);
+
+        ctx.window->draw();
+    }
+};
+
+class Scene {
+private:
+    std::vector<Object<sf::Drawable>> objects = std::vector<Object<sf::Drawable>>();
+
+public:
+    auto add(Object o) -> void {
+        objects.push_back(o);
+    }
+
+    auto render(Context& ctx) -> void {
+        ctx.window->clear();
+        for (auto object : objects) {
+            object.render(ctx);
+        }
+    }
 };
 
 auto main() -> int {
     fmt::println("Hello world!");
 
     auto window = sf::RenderWindow(
-            sf::VideoMode({800, 600}), "Test",
+            sf::VideoMode({800, 600}), "Pjc_project",
             sf::Style::Default, sf::ContextSettings(0, 0, 8)
     );
+    auto ctx = Context();
+    ctx.window = &window;
+
+    auto scene = Scene();
+
     while (window.isOpen()) {
         auto event = sf::Event();
         while (window.pollEvent(event)) {
